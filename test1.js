886503c1cee5c74938d5b97806fc3d3c32c47688
hrb1975 <hrb1975@github.com>
2024-08-25 07:31:13 +0530

    all

--- test1.js
+++ test1.js
@@ -3,4 +3,10 @@ for (i=0;i<10;i++){
     console.log("LEARN BRANCES");
     console.log("learnbranches");
     console.log("PULL REQUEST");
+<<<<<<< HEAD
+=======
+    console.log("cherry pick");
+    console.log("cherrypick");
+    console.log("cherrypick123");
+>>>>>>> ee0695b (cherrypicktestfinal)
 }
